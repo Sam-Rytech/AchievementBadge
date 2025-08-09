e70203e95aa0c7b75080ffa308e1095a178bd2e2
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-10 00:23:20 +0100

    Connect wallet function

--- pages/index.js
+++ pages/index.js
@@ -10,4 +10,21 @@ export default function Home() {
     const [message, setMessage] = useState('')
 
     const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+
+    async function connectWallet() {
+      if (!window.ethereum) {
+        alert('Please install MetaMask!')
+        return
+      }
+      try {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        })
+        setAccount(accounts[0])
+        setMessage('')
+      } catch (error) {
+        setMessage('Wallet connection failed')
+        console.error(error)
+      }
+    }
 }
