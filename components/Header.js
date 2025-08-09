6ee019de5de64528f2bb3d12a40b05768944cb0a
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-10 00:21:19 +0100

    Header component added

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="py-6 bg-gradient-to-r from-indigo-600 to-purple-600 text-white text-center font-bold text-3xl">
+      NFT Achievement Badge
+    </header>
+  )
+}
