3024b9ebdc2a00b1d44ddd63ba5d0763d756b1c4
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-10 00:22:46 +0100

    constants defined

--- pages/index.js
+++ pages/index.js
@@ -3,3 +3,11 @@ import { ethers } from 'ethers'
 import AchievementBadgeABI from '../lib/AchievementBadge.json'
 import Button from '../components/Button'
 import Header from '../components/Header'
+
+export default function Home() {
+    const [account, setAccount] = useState(null)
+    const [minting, setMinting] = useState(false)
+    const [message, setMessage] = useState('')
+
+    const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+}
